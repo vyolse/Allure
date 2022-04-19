3df457a606c06fea7f38d5be0dc2547d5b0c4176
vyolse <vyolse@gmail.com>
2022-04-19 17:11:24 +0300

    Lesson15

---
+++ src/test/java/test/WebDriverSettings.java
@@ -0,0 +1,29 @@
+package test;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSettings {
+    public static WebDriver driver;
+    public static LoginPage loginPage;
+
+    @BeforeAll
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("bbc"));
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+}
